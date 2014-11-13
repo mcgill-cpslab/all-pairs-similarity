d467da3a82e163e061f8ab180610bd942f8aed05
CodingCat <zhunansjtu@gmail.com>
2014-11-12 23:03:52 -0500

    dispatch read request in SimilaritySearchService

--- src/main/scala/cpslab/deploy/SimilaritySearchService.scala
+++ src/main/scala/cpslab/deploy/SimilaritySearchService.scala
@@ -2,20 +2,45 @@ package cpslab.deploy
 
 import akka.actor.{Props, Actor}
 import akka.routing.FromConfig
+import com.typesafe.config.Config
+import org.apache.hadoop.hbase.util.Bytes
+
 import cpslab.message.GetInputRequest
 
-class SimilaritySearchService extends Actor {
+class SimilaritySearchService(conf: Config) extends Actor {
 
   // the router actor
   val workerRouter = context.actorOf(FromConfig.props(Props[SimilarityWorker]),
     name = "workerRouter")
 
+  val parallelism = conf.getInt("cpslab.allpair.parallelism")
+
   def receive: Receive = {
-    case getRequest @ GetInputRequest(_, _, _) =>
-      workerRouter ! getRequest
+    case GetInputRequest(table, startRowKey, endRowKey) =>
+      val startRowKeyInt = Bytes.toInt(startRowKey)
+      val endRowKeyInt = Bytes.toInt(endRowKey)
+      var newStartKeyInt = startRowKeyInt
+      while (newStartKeyInt <= endRowKeyInt) {
+        val stepLength = {
+          if (newStartKeyInt + stepLength > endRowKeyInt) {
+            endRowKeyInt - newStartKeyInt + 1
+          } else {
+            (endRowKeyInt - startRowKeyInt + 1) / parallelism
+          }
+        }
+        val newEndKeyInt = newStartKeyInt + stepLength - 1
+        workerRouter ! GetInputRequest(table, Bytes.toBytes(newStartKeyInt),
+          Bytes.toBytes(newEndKeyInt))
+        newStartKeyInt = newStartKeyInt + stepLength
+        //FSM
+        context.become(processing)
+      }
+  }
+
+  def processing: Receive = {
+    null
   }
 }
-
 
 object SimilaritySearchService {
 
