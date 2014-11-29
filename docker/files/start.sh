d46b77f935d23664892001926061a84bbd9c4b07
CodingCat <zhunansjtu@gmail.com>
2014-11-28 20:49:47 -0500

    fix docker scripts

--- docker/files/start.sh
+++ docker/files/start.sh
@@ -3,3 +3,8 @@
 cd app;
 
 java -cp core/target/scala-2.10/AllPairsSimilarityCore-assembly-0.1.jar cpslab.service.SimilaritySearchService conf/cluster.conf conf/deploy.conf conf/app.conf
+
+while [ 1 ];
+do
+  sleep 3
+done
