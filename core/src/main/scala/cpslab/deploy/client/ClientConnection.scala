d55fd771c1cb58110b9d57ac1a0e6ebd610da9ab
CodingCat <zhunansjtu@gmail.com>
2015-01-16 14:29:20 -0500

    change ClientConnection API

--- core/src/main/scala/cpslab/deploy/client/ClientConnection.scala
+++ core/src/main/scala/cpslab/deploy/client/ClientConnection.scala
@@ -1,14 +1,29 @@
 package cpslab.deploy.client
 
+import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
 import akka.actor.ActorSystem
 import cpslab.message.VectorIOMsg
 import org.apache.spark.mllib.linalg.{SparseVector => SparkSparseVector}
 
-class ClientConnection(remoteAddress: List[String], system: ActorSystem) {
+class ClientConnection(remoteAddresses: List[String], remoteActorSystemName: String, 
+                       localActorSystem: ActorSystem) {
+  
+  val remoteAddressRouterList = {
+    val brokerList = new ListBuffer[String]
+    for (hostPortPair <- remoteAddresses) {
+      val (remoteHost, remotePort) = {
+        val hostAndPort = hostPortPair.split(":")
+        (hostAndPort(0), hostAndPort(1))
+      }
+      brokerList += s"akka.tcp://$remoteActorSystemName@$remoteHost:$remotePort/user/regionRouter"
+    }
+    brokerList.toList
+  }
 
-  val remoteRouter = system.actorSelection(remoteAddress(Random.nextInt(remoteAddress.length)))
+  val remoteRouter = localActorSystem.actorSelection(remoteAddresses(
+    Random.nextInt(remoteAddresses.length)))
 
   /**
    * send the set of sparseVector to remote cluster
