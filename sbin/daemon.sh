17b469fa9dd210e25b5f437d5596a76289be9ee6
CodingCat <zhunansjtu@gmail.com>
2015-01-25 12:23:02 -0500

    save log in starting script

--- sbin/daemon.sh
+++ sbin/daemon.sh
@@ -6,12 +6,15 @@ WORKDIR=`pwd`
 PID_FILE="$WORKDIR/all-pairs-similarity/pid/sim.pid"
 option=$1
 
-case $option in 
+STARTTIME=`date+%s%N | cut -b1-13`
+
+case $option in
 	(start)
 	  mkdir -p $WORKDIR/all-pairs-similarity/pid/
+		mkdir -p $WORKDIR/all-pairs-similarity/logs/
 	  touch $PID_FILE
 	  cd $WORKDIR/all-pairs-similarity
-	  nohup java -Xmx4096m -Xms1024m -cp core/target/scala-2.10/AllPairsSimilarityCore-assembly-0.1.jar cpslab.deploy.server.SimilaritySearchService conf/akka.conf conf/app.conf >> /dev/null &
+	  nohup java -Xmx4096m -Xms1024m -cp core/target/scala-2.10/AllPairsSimilarityCore-assembly-0.1.jar cpslab.deploy.server.SimilaritySearchService conf/akka.conf conf/app.conf > logs/similarity-$STARTTIME &
 	  pid=$!
 	  echo $pid > $PID_FILE
 	;;
@@ -20,4 +23,4 @@ case $option in
 	  pid=`cat $WORKDIR/all-pairs-similarity/pid/sim.pid`
 	  kill -9 $pid
 	;;
-esac	
+esac
